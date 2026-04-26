b53ad99eeef1af1e66e89d23dc46ecdefe99f6b4
abent01 <ahmed.bentaher@estudiantat.upc.edu>
2026-04-26 23:33:24 +0200

    Homogeneizar Pantalla-Mapa

---
+++ FacadeModel.py
@@ -0,0 +1,24 @@
+from SolicitudOpenMeteo import SolicitudOpenMeteo
+
+class FacadeModel:
+    """ Patrón de diseño Facade (Fachada) para Model:
+        Agrupar todos los metodos de las clases lógicas en una sola clase.
+    """
+    def __init__(self):
+        self.servicio = SolicitudOpenMeteo()
+
+    def buscar_ciudad_con_clima(self, nombre_ciudad):
+        ciudad = self.servicio.obtener_ciudad_con_clima(nombre_ciudad)
+        return ciudad
+
+    def buscar_punto_con_clima(self, latitud, longitud):
+        ciudad = self.servicio.obtener_punto_con_clima(latitud, longitud)
+        return ciudad
+
+    def facade_buscar_ciudades(self, texto_ciudad):
+        lista_ciudades = self.servicio.buscar_ciudades(texto_ciudad)
+        return lista_ciudades
+
+    def facade_actualizar_param(self, nombre_ciudad):
+        dataframe = self.servicio.obtener_prevision_horaria(nombre_ciudad)
+        return dataframe
--- main_grafico.py
+++ main_grafico.py
@@ -1,12 +1,11 @@
 import tkinter as tk
-
-from Mapa import Mapa
-from presenter_ventana2 import PresenterVentana2
 from view_ventana2 import ViewVentana2
+from presenter_ventana2 import PresenterVentana2
+from FacadeModel import FacadeModel
 
 
-ventana = tk.Tk()
-modelo = Mapa()
-vista = ViewVentana2(ventana)
+ventana2 = tk.Tk()
+modelo = FacadeModel()
+vista = ViewVentana2(ventana2)
 PresenterVentana2(vista, modelo)
-ventana.mainloop()
+ventana2.mainloop()
